68dc78eada1a89b90f6ad9882f753cafcb3c0647
Lisa <bardelli.lisa@gmail.com>
2021-03-10 14:58:29 +0000

    Add result of review

--- Review_3/lib/spelling.rb
+++ Review_3/lib/spelling.rb
@@ -1,34 +1,40 @@
 def check_spelling(string) 
-
     dictonary = ["The", "Hello", "World"]
-
     array = string.split(" ")
-
     result = ""
 
     last_element = array.last
-    count_word = array.length
-    array.each do |word|
-         if word != last_element
-       if  !dictonary.include? (word)
-        word = "~"+word+"~"+" "
-        result += word
-       else 
-        word 
-        result += word + " "
-    end 
-        else 
-            if  !dictonary.include? (word)
-                word = "~"+word+"~"
-                result += word
-               else 
-                word 
-                result += word
-               end 
-        end 
+
+    array.each do |word| 
+
+            if  include?(word)
+                word = "~"+word+"~"+" " if word != last_element
+                word = "~"+word+"~" if word == last_element
+        
+            else 
+                result += word + " " if word != last_element
+                result += word if word == last_element
+                end 
+        
     end 
 
     return result 
 
+
+end 
+
+
+
+def include?(string)
+    dictonary = ["The", "Hello", "World"]
+    array = string.split(" ")
+
+    array.each do |word|
+    if  !dictonary.include? (word)
+        return true
+       else 
+        return false
+       end 
+    end 
 
 end 
