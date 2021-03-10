bd6798cf8315f10e127c555af8f0fe78aa7076c7
Lisa <bardelli.lisa@gmail.com>
2021-03-10 14:42:07 +0000

    3 tests passing

--- Review_3/lib/spelling.rb
+++ Review_3/lib/spelling.rb
@@ -6,14 +6,26 @@ def check_spelling(string)
 
     result = ""
 
+    last_element = array.last
+    count_word = array.length
     array.each do |word|
+         if word != last_element
        if  !dictonary.include? (word)
-        word = "~"+word+"~"
+        word = "~"+word+"~"+" "
         result += word
        else 
-    word 
-        result += word
+        word 
+        result += word + " "
     end 
+        else 
+            if  !dictonary.include? (word)
+                word = "~"+word+"~"
+                result += word
+               else 
+                word 
+                result += word
+               end 
+        end 
     end 
 
     return result 
